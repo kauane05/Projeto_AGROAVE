647b6cce9756c9731374d3b5ba3d87b38ebeb59b
kauane05 <126490321+kauane05@users.noreply.github.com>
2023-05-03 00:31:59 -0400

    Tarefa Completa

---
+++ AppInterfaces/AppInterfaces/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace AppInterfaces
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btCadastrarCliente(object sender, RoutedEventArgs e)
+        {
+            CadastrarCliente form = new CadastrarCliente();
+            form.ShowDialog();
+        }
+
+        private void btCadastrarEstoqueClick(object sender, RoutedEventArgs e)
+        {
+            CadastrarEstoqueProducao form = new CadastrarEstoqueProducao();
+            form.ShowDialog();
+        }
+
+        private void btControlarAcesso_Click(object sender, RoutedEventArgs e)
+        {
+            ControlarAcessoAoPerfil form = new ControlarAcessoAoPerfil();
+            form.ShowDialog();
+        }
+
+        private void btCadastrarFornecedor_Click(object sender, RoutedEventArgs e)
+        {
+            CadastrarFornecedor1 form = new CadastrarFornecedor1();
+            form.ShowDialog();  
+        }
+    }
+}
